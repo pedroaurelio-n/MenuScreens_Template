3e97af846b7c27dfb07986fd339a78c3495246e1
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2022-09-26 17:13:04 -0300

    Created scripts for MenuController and MenuPage. Imported ScriptTemplate from another project. Organized Folders

---
+++ Assets/_src/Scripts/MenuController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+namespace PedroAurelio.MenuScreens
+{
+    public class MenuController : MonoBehaviour
+    {
+        
+    }
+}
